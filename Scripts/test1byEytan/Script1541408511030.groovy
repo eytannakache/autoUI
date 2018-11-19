d6a5bcf3d48e60e4a81b71cdab80a58c9952260a
enakache <enakache@10.0.11.86>
2018-11-19 11:42:10 +0200

    new1 try1

--- Scripts/test1byEytan/Script1541408511030.groovy
+++ Scripts/test1byEytan/Script1541408511030.groovy
@@ -13,3 +13,8 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+println (GlobalVariable.card)
+//println(webs)
+
+//WebUI.openBrowser(webs)
+
---
+++ Test Suites/suitEN.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>suitEN</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-11-06T10:46:17</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fbb54a50-9516-4cee-99d7-ef21132e672a</testSuiteGuid>
+   <testCaseLink>
+      <guid>c6e1fb9b-e0bf-4044-907f-8799e26efecb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test1byEytan</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>4ba611e5-2d41-40ba-88cd-07bc40571c22</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/excelFile1</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>4ba611e5-2d41-40ba-88cd-07bc40571c22</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>website</value>
+         <variableId>b2c32439-4d71-4d35-a763-e7c10b1457d0</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
